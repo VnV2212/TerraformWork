afce6a5df8579821f8a2b0e8cfb1c7e043a718c7
VnV <49614676+VnV2212@users.noreply.github.com>
2022-06-22 11:24:51 +0530

    Update main.tf

--- ModularApproach/main.tf
+++ ModularApproach/main.tf
@@ -1,3 +1,109 @@
+# data "aws_availability_zones" "all" {
+# }
+
+# resource "aws_security_group" "instance" {
+#   name = "terraform-example-instance"
+  
+#   ingress {
+#     from_port	  = "${var.server_port}"
+#     to_port		  = "${var.server_port}"
+#     protocol	  = "tcp"
+#     cidr_blocks	= ["0.0.0.0/0"]
+#   }
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# resource "aws_security_group" "elb" {
+#   name = "terraform-example-elb"
+  
+#   ingress {
+#     from_port	  = 80
+# 	  to_port		  = 80
+# 	  protocol	  = "tcp"
+# 	  cidr_blocks	= ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port	  = 0
+# 	  to_port		  = 0
+# 	  protocol	  = "-1"
+# 	  cidr_blocks	= ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_launch_configuration" "example" {
+#   image_id		    = "ami-0f2e255ec956ade7f"
+#   instance_type   = "t2.micro"
+#   security_groups = ["${aws_security_group.instance.id}"]
+  
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p "${var.server_port}" &
+#               EOF
+			  
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# resource "aws_autoscaling_group" "example" {
+#   launch_configuration = "${aws_launch_configuration.example.id}"
+#   availability_zones   = ["${data.aws_availability_zones.all.names[0]}"]
+  
+#   load_balancers       = ["${aws_elb.example.name}"]
+#   health_check_type    = "ELB"
+  
+#   min_size = 2
+#   max_size = 10
+  
+# }
+
+# resource "aws_elb" "example" {
+#   name               = "terraform-asg-example"
+#   availability_zones = ["${data.aws_availability_zones.all.names[1]}"]
+#   security_groups    = ["${aws_security_group.elb.id}"]
+  
+#   listener {
+#     lb_port           = 80
+#     lb_protocol       = "http"
+#     instance_port     = "${var.server_port}"
+#     instance_protocol = "http"
+#   }
+  
+#   health_check {
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 3
+#     interval            = 30
+#     target              = "HTTP:${var.server_port}/"
+#   }
+# }
+
+# resource "aws_s3_bucket" "mybucket" {
+#   bucket = "mybucketvnv"
+# }
+#################################################
+
+# VPC
+# resource "aws_vpc" "default" {
+#   cidr_block = "10.0.0.0/16"
+# }
+
+# # internet gateway
+# resource "aws_internet_gateway" "default" {
+#   vpc_id = aws_vpc.default.id
+# }
+
+# resource "aws_route" "internet_access" {
+#   route_table_id         = aws_vpc.default.main_route_table_id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.default.id
+# }
+
 module "base" {
   source = "Users/vatsalparmar/terraform-course/module"
   cidr = "10.0.0.0/16"
